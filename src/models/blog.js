9ed32fc86e286c32ad4cf85b4372f05a55694262
张子元 <zhangziyuand@gmail.com>
2018-09-13 10:31:28 +0800

    feat: 升级，添加时间戳

--- src/models/blog.js
+++ src/models/blog.js
@@ -3,7 +3,7 @@ import { Base64 } from "../common/libs/base64"
 
 class Blog extends Base {
 	getArticleList(data = {}, allowCache = true) {
-		return this.get('list', data, allowCache)
+		return this.get(`list?${new Date().getTime()}`, data, allowCache)
 	}
 	getDetail(id, data = {}, allowCache = true) {
 		return this.get(`article/detail/${id}`, data, allowCache)
